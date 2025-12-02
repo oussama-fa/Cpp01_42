085b31f93337186c5811929c43acb3586e55b219
Oussama Farah <oufarah@e1r5p2.1337.ma>
2025-12-02 10:23:31 +0100

    Done

---
+++ ex06/Harl.cpp
@@ -0,0 +1,46 @@
+#include "Harl.hpp"
+
+void	Harl::debug( void ) {
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+void	Harl::info( void ) {
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+void	Harl::warning( void ) { 	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void	Harl::error( void ) { 	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int flag = 0;
+
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			flag = i + 1;
+			break ;
+		}
+	}
+	switch (flag) {
+		case 1:
+			debug();
+			break;
+		case 2:
+			info();
+			break;
+		case 3:
+			warning();
+			break;
+		case 4:
+			error();
+			break;
+		default:
+			std::cout << "Wrong Input ! This " << level << " is Invalid\n";
+			break;
+	}
+}
